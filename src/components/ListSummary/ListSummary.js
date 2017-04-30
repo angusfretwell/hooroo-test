ed957f8374a2eaa897784e1f7a06c38949259301
Angus Fretwell <angusfretwell@me.com>
2017-05-01 00:57:48 +1000

    feat: add ListSummary component

---
+++ src/components/ListSummary/ListSummary.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './ListSummary.css'
+
+export default ({ results, location }) => (
+  <p className="ListSummary">
+    <strong>{results}</strong> hotels in <strong>{location}</strong>.
+  </p>
+);
