b39849ea684471625e0de4c97c8f6a74b10576c8
Angus Fretwell <angusfretwell@me.com>
2017-05-01 00:59:44 +1000

    feat: render List in App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import Header from '../Header';
+import List from '../List';
 import './App.css';
 
 export default () => (
   <div className="App">
     <Header />
+    <List />
   </div>
 );
