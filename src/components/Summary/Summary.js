5366c7da242772e0d6486fadd93031b587ae549a
Angus Fretwell <angusfretwell@me.com>
2017-05-02 22:44:18 +1000

    feat: add Summary component

---
+++ src/components/Summary/Summary.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Rating from '../Rating';
+import './Summary.css';
+import tick from './tick.svg';
+
+export default () => (
+  <div className="Summary">
+    <div>
+      <h2 className="Summary-title">Primus Hotel Sydney</h2>
+      <address className="Summary-address">339 Pitt St, Sydney</address>
+    </div>
+
+    <Rating rating={3} />
+
+    <p className="Summary-type">Deluxe king</p>
+    <p className="Summary-cancellation">
+      <i className="fa fa-check-square-o"></i> Free cancellation
+    </p>
+  </div>
+);
