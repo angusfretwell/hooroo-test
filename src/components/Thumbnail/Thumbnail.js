09f8c9c97e3cf4056148e005ee7701693d5b0912
Angus Fretwell <angusfretwell@me.com>
2017-05-02 22:43:48 +1000

    feat: add Thumbnail component

---
+++ src/components/Thumbnail/Thumbnail.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Thumbnail.css'
+
+export default () => (
+  <div className="Thumbnail">
+    <p className="Thumbnail-callout">
+      Exclusive Deal
+    </p>
+
+    <div className="Thumbnail-wrapper">
+      <img src="//placehold.it/145x125" className="Thumbnail-image" />
+    </div>
+  </div>
+);
