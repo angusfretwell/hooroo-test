31af03ef2f7dde0b0ebe9dd777650a24bbd1f980
Angus Fretwell <angusfretwell@me.com>
2017-05-03 22:34:57 +1000

    test: add spec for Thumbnail

---
+++ src/components/Thumbnail/Thumbnail.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Thumbnail from '../Thumbnail';
+
+describe('<Thumbnail />', () => {
+  it('renders the promotion label', () => {
+    expect(shallow(<Thumbnail promotion={'Red Hot'} />).text()).toBe('Red Hot');
+  })
+
+  it('renders the image', () => {
+    const wrapper = shallow(<Thumbnail image={'//example.com/image.jpg'} />);
+    expect(wrapper.find('img').props().src).toBe('//example.com/image.jpg');
+  });
+});
